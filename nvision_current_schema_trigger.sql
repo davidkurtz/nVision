422181c5442bac56e5e2b4a9174e8395bfe085b3
davidkurtz <info@go-faster.co.uk>
2022-12-05 17:02:27 +0000

    Update nvision_current_schema_trigger.sql

--- nvision_current_schema_trigger.sql
+++ nvision_current_schema_trigger.sql
@@ -1,8 +1,12 @@
 REM nvision_current_schema_trigger.sql
+set serveroutput on echo on
 spool nvision_current_schema_trigger
 --------------------------------------------------------------------------------
 --nvision set current schema 
 --------------------------------------------------------------------------------
+rollback;
+alter session set current_schema = sysadm;
+
 CREATE OR REPLACE TRIGGER sysadm.nvision_current_schema
 BEFORE UPDATE OF runstatus ON sysadm.psprcsrqst
 FOR EACH ROW
@@ -14,4 +18,29 @@ EXCEPTION WHEN OTHERS THEN NULL; --exception deliberately coded to suppress all 
 END;
 /
 show errors
+spool off
+DECLARE 
+  l_prcsinstance INTEGER := 0;
+  l_current_schema VARCHAR2(30);
+BEGIN  
+  select sys_context('userenv','current_schema') 
+  into l_current_schema
+  from dual;
+  dbms_output.put_line('PI='||l_prcsinstance||', current_schema='||l_current_schema);
+
+  UPDATE psprcsrqst
+  SET runstatus = 7
+  WHERE prcsname = 'RPTBOOK'
+  and prcstype like 'nVision%'
+  and rownum = 1
+  RETURN prcsinstance INTO l_prcsinstance;
+  
+  select sys_context('userenv','current_schema') 
+  into l_current_schema
+  from dual;
+  dbms_output.put_line('PI='||l_prcsinstance||', current_schema='||l_current_schema);
+END;
+/
+
+rollback;
 spool off
